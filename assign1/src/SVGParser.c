010576eaa99ea68778d3d2caf3a30ae611b984f5
= <=>
2020-01-20 22:53:48 -0500

    Reformat code

--- assign1/src/SVGParser.c
+++ assign1/src/SVGParser.c
@@ -4,31 +4,31 @@
 
 //Public API
 
-SVGimage* createSVGimage(char* fileName){
-    SVGimage *image = calloc(1, sizeof(SVGimage));
+SVGimage* createSVGimage(char* fileName) {
+    SVGimage* image = calloc(1, sizeof(SVGimage));
 
-    xmlDoc *document = xmlReadFile(fileName, NULL, 0);
+    xmlDoc* document = xmlReadFile(fileName, NULL, 0);
     //Return NULL if the parsing failed
     if (document == NULL) return NULL;
 
-    xmlNode *rootNode = xmlDocGetRootElement(document);
+    xmlNode* rootNode = xmlDocGetRootElement(document);
 
     //Get namespace
-    strcpy(image->namespace, (char *)rootNode->nsDef->href);
+    strcpy(image->namespace, (char*)rootNode->nsDef->href);
 
     //Get title, if it exists
-    xmlNode *titleNode = rootNode->children;
+    xmlNode* titleNode = rootNode->children;
     do {
         titleNode = titleNode->next;
-    } while (titleNode != NULL && strcmp("title", (char *)titleNode->name) != 0);
-    strcpy(image->title,titleNode == NULL ? "" : (char *)titleNode->children->content);
+    } while (titleNode != NULL && strcmp("title", (char*)titleNode->name) != 0);
+    strcpy(image->title, titleNode == NULL ? "" : (char*)titleNode->children->content);
 
     //Get description
-    xmlNode *descNode = rootNode->children;
+    xmlNode* descNode = rootNode->children;
     do {
         descNode = descNode->next;
-    } while (descNode != NULL && strcmp("desc", (char *)descNode->name) != 0);
-    strcpy(image->description,descNode == NULL ? "" : (char *)descNode->children->content);
+    } while (descNode != NULL && strcmp("desc", (char*)descNode->name) != 0);
+    strcpy(image->description, descNode == NULL ? "" : (char*)descNode->children->content);
     //TODO: Generalize title and description functions
 
     image->rectangles = populateRects(rootNode, initializeList(rectangleToString, deleteRectangle, compareRectangles));
@@ -42,8 +42,8 @@ SVGimage* createSVGimage(char* fileName){
     return image;
 }
 
-char* SVGimageToString(SVGimage* img){
-    char *desc = calloc(5120, sizeof(char));
+char* SVGimageToString(SVGimage* img) {
+    char* desc = calloc(5120, sizeof(char));
     sprintf(desc, "Namespace: %s\nTitle: %s\nDescription: %s", img->namespace, img->title, img->description);
 
     char* listDesc;
@@ -74,7 +74,7 @@ char* SVGimageToString(SVGimage* img){
     return desc;
 }
 
-void deleteSVGimage(SVGimage* img){
+void deleteSVGimage(SVGimage* img) {
     if (img == NULL) return;
 
     freeList(img->rectangles);
@@ -85,29 +85,29 @@ void deleteSVGimage(SVGimage* img){
     free(img);
 }
 
-List* getRects(SVGimage* img){
+List* getRects(SVGimage* img) {
     return img == NULL ? NULL : img->rectangles;
 }
 
-List* getCircles(SVGimage* img){
+List* getCircles(SVGimage* img) {
     return img == NULL ? NULL : img->circles;
 }
 
-List* getGroups(SVGimage* img){
+List* getGroups(SVGimage* img) {
     return img == NULL ? NULL : img->groups;
 }
 
-List* getPaths(SVGimage* img){
+List* getPaths(SVGimage* img) {
     return img == NULL ? NULL : img->paths;
 }
 
-int numRectsWithArea(SVGimage* img, float area){
+int numRectsWithArea(SVGimage* img, float area) {
     int count = 0;
 
     if (img != NULL && img->rectangles->length > 0) {
         Node* currentRect = img->rectangles->head;
         for (int i = img->rectangles->length; i >= 0; i--) {
-            if ((((Rectangle *) currentRect->data)->width * ((Rectangle *) currentRect->data)->height) == area) {
+            if ((((Rectangle*)currentRect->data)->width * ((Rectangle*)currentRect->data)->height) == area) {
                 count++;
             }
             //TODO: Make sure this is next and not previous
@@ -118,13 +118,13 @@ int numRectsWithArea(SVGimage* img, float area){
     return count;
 }
 
-int numCirclesWithArea(SVGimage* img, float area){
+int numCirclesWithArea(SVGimage* img, float area) {
     int count = 0;
 
     if (img != NULL && img->circles->length > 0) {
         Node* currentCircle = img->circles->head;
         for (int i = img->circles->length; i >= 0; i--) {
-            if ((((Circle *) currentCircle->data)->r * ((Circle *) currentCircle->data)->r * PI) == area) {
+            if ((((Circle*)currentCircle->data)->r * ((Circle*)currentCircle->data)->r * PI) == area) {
                 count++;
             }
             //TODO: Make sure this is next and not previous
@@ -135,13 +135,13 @@ int numCirclesWithArea(SVGimage* img, float area){
     return count;
 }
 
-int numPathsWithdata(SVGimage* img, char* data){
+int numPathsWithdata(SVGimage* img, char* data) {
     int count = 0;
 
     if (img != NULL && img->paths->length > 0) {
         Node* currentPath = img->paths->head;
         for (int i = img->paths->length; i >= 0; i--) {
-            if (strcmp(((Path *)currentPath)->data, data) == 0) {
+            if (strcmp(((Path*)currentPath)->data, data) == 0) {
                 count++;
             }
             //TODO: Make sure this is next and not previous
@@ -152,16 +152,16 @@ int numPathsWithdata(SVGimage* img, char* data){
     return count;
 }
 
-int numGroupsWithLen(SVGimage* img, int len){
+int numGroupsWithLen(SVGimage* img, int len) {
     int count = 0;
 
-    if (img != NULL && img->groups->length > 0){
+    if (img != NULL && img->groups->length > 0) {
         Node* currentGroup = img->groups->head;
         for (int i = img->paths->length; i >= 0; i--) {
-            int currentGroupLength = ((Group *)currentGroup->data)->rectangles->length +
-                                     ((Group *)currentGroup->data)->circles->length +
-                                     ((Group *)currentGroup->data)->paths->length +
-                                     ((Group *)currentGroup->data)->groups->length;
+            int currentGroupLength = ((Group*)currentGroup->data)->rectangles->length +
+                                     ((Group*)currentGroup->data)->circles->length +
+                                     ((Group*)currentGroup->data)->paths->length +
+                                     ((Group*)currentGroup->data)->groups->length;
 
             if (currentGroupLength == len) {
                 count++;
@@ -176,27 +176,27 @@ int numGroupsWithLen(SVGimage* img, int len){
 
 //Helper Functions
 
-int numAttr(SVGimage* img){
+int numAttr(SVGimage* img) {
     //TODO: Loop through rects, circles, paths, groups, attribute lists for this (INCLUDES SVG NODE ATTRIBUTES (NOT including the description, title, or namespace))
     return 0;
 }
 
-void deleteAttribute( void* data){
+void deleteAttribute(void* data) {
 
 }
 
-char* attributeToString( void* data){
+char* attributeToString(void* data) {
     char* temp = calloc(14, sizeof(char));
     strcpy(temp, "<PLACEHOLDER>");
     return temp;
 }
 
 //Unused
-int compareAttributes(const void *first, const void *second){
+int compareAttributes(const void* first, const void* second) {
     return 0;
 }
 
-void deleteGroup(void* data){
+void deleteGroup(void* data) {
     if (((Group*)data)->rectangles != NULL) freeList(((Group*)data)->rectangles);
     if (((Group*)data)->circles != NULL) freeList(((Group*)data)->circles);
     if (((Group*)data)->paths != NULL) freeList(((Group*)data)->paths);
@@ -204,88 +204,88 @@ void deleteGroup(void* data){
     free(data);
 }
 
-char* groupToString( void* data){
+char* groupToString(void* data) {
     char* temp = calloc(14, sizeof(char));
     strcpy(temp, "<PLACEHOLDER>");
     return temp;
 }
 
 //Unused
-int compareGroups(const void *first, const void *second){
+int compareGroups(const void* first, const void* second) {
     return 0;
 }
 
-void deleteRectangle(void* data){
+void deleteRectangle(void* data) {
     free(data);
 }
 
-char* rectangleToString(void* data){
+char* rectangleToString(void* data) {
     char* temp = calloc(14, sizeof(char));
     strcpy(temp, "<PLACEHOLDER>");
     return temp;
 }
 
 //Unused
-int compareRectangles(const void *first, const void *second){
+int compareRectangles(const void* first, const void* second) {
     return 0;
 }
 
-void deleteCircle(void* data){
+void deleteCircle(void* data) {
     free(data);
 }
 
-char* circleToString(void* data){
+char* circleToString(void* data) {
     char* temp = calloc(14, sizeof(char));
     strcpy(temp, "<PLACEHOLDER>");
     return temp;
 }
 
 //Unused
-int compareCircles(const void *first, const void *second){
+int compareCircles(const void* first, const void* second) {
     return 0;
 }
 
-void deletePath(void* data){
+void deletePath(void* data) {
     freeList(((Path*)data)->otherAttributes);
     free(data);
 }
 
-char* pathToString(void* data){
+char* pathToString(void* data) {
     char* temp = calloc(14, sizeof(char));
     strcpy(temp, "<PLACEHOLDER>");
     return temp;
 }
 
 //Unused
-int comparePaths(const void *first, const void *second){
+int comparePaths(const void* first, const void* second) {
     return 0;
 }
 
-List* populateRects (xmlNode* rootNode, List* list){
+List* populateRects(xmlNode* rootNode, List* list) {
     for (xmlNode* node = rootNode; node != NULL; node = node->next) {
         if (node->children != NULL) populateRects(rootNode->children, list);
 
         if (strcmp((char*)node->name, "rect") != 0) continue;
 
-        Rectangle *rectToAdd = calloc(1, sizeof(Rectangle));
+        Rectangle* rectToAdd = calloc(1, sizeof(Rectangle));
 
-        char* value = (char*)xmlGetProp(node, (xmlChar *)"x");
+        char* value = (char*)xmlGetProp(node, (xmlChar*)"x");
         rectToAdd->x = value == NULL ? 0 : split(value, NULL);
         xmlFree(value);
 
-        value = (char*)xmlGetProp(node, (xmlChar *)"y");
+        value = (char*)xmlGetProp(node, (xmlChar*)"y");
         rectToAdd->y = value == NULL ? 0 : split(value, NULL);
         xmlFree(value);
 
-        value = (char*)xmlGetProp(node, (xmlChar *)"width");
+        value = (char*)xmlGetProp(node, (xmlChar*)"width");
         rectToAdd->width = value == NULL ? 0 : split(value, NULL);
         xmlFree(value);
 
-        value = (char*)xmlGetProp(node, (xmlChar *)"height");
+        value = (char*)xmlGetProp(node, (xmlChar*)"height");
         rectToAdd->height = value == NULL ? 0 : split(value, NULL);
         xmlFree(value);
 
-        value = (char*)xmlGetProp(node, (xmlChar *)"units");
+        value = (char*)xmlGetProp(node, (xmlChar*)"units");
         if (value != NULL) split(value, rectToAdd->units);
         xmlFree(value);
 
@@ -298,27 +298,27 @@ List* populateRects (xmlNode* rootNode, List* list){
     return list;
 }
 
-List* populateCircles (xmlNode* rootNode, List* list){
+List* populateCircles(xmlNode* rootNode, List* list) {
     for (xmlNode* node = rootNode; node != NULL; node = node->next) {
         if (node->children != NULL) populateCircles(rootNode->children, list);
 
         if (strcmp((char*)node->name, "circle") != 0) continue;
 
-        Circle *circleToAdd = calloc(1, sizeof(Circle));
+        Circle* circleToAdd = calloc(1, sizeof(Circle));
 
-        char* value = (char*)xmlGetProp(node, (xmlChar *)"cx");
+        char* value = (char*)xmlGetProp(node, (xmlChar*)"cx");
         circleToAdd->cx = value == NULL ? 0 : split(value, NULL);
         xmlFree(value);
 
-        value = (char*)xmlGetProp(node, (xmlChar *)"cy");
+        value = (char*)xmlGetProp(node, (xmlChar*)"cy");
         circleToAdd->cy = value == NULL ? 0 : split(value, NULL);
         xmlFree(value);
 
-        value = (char*)xmlGetProp(node, (xmlChar *)"r");
+        value = (char*)xmlGetProp(node, (xmlChar*)"r");
         circleToAdd->r = value == NULL ? 0 : split(value, NULL);
         xmlFree(value);
 
-        value = (char*)xmlGetProp(node, (xmlChar *)"units");
+        value = (char*)xmlGetProp(node, (xmlChar*)"units");
         if (value != NULL) split(value, circleToAdd->units);
         xmlFree(value);
 
@@ -331,15 +331,15 @@ List* populateCircles (xmlNode* rootNode, List* list){
     return list;
 }
 
-List* populatePaths (xmlNode* rootNode, List* list){
+List* populatePaths(xmlNode* rootNode, List* list) {
     for (xmlNode* node = rootNode; node != NULL; node = node->next) {
         if (node->children != NULL) populatePaths(rootNode->children, list);
 
         if (strcmp((char*)node->name, "path") != 0) continue;
 
-        Path *pathToAdd = calloc(1, sizeof(Path));
+        Path* pathToAdd = calloc(1, sizeof(Path));
 
-        char* value = (char*)xmlGetProp(node, (xmlChar *)"d");
+        char* value = (char*)xmlGetProp(node, (xmlChar*)"d");
         pathToAdd->data = calloc(strlen(value) + 1, sizeof(char));
         strcpy(pathToAdd->data, value);
         xmlFree(value);
@@ -353,16 +353,16 @@ List* populatePaths (xmlNode* rootNode, List* list){
     return list;
 }
 
-List* populateGroups (xmlNode* rootNode, List* list){
+List* populateGroups(xmlNode* rootNode, List* list) {
 
     return list;
 }
 
-List* populateAttr (xmlNode* rootNode, List* list){
+List* populateAttr(xmlNode* rootNode, List* list) {
 
     return list;
 }
 
-float split (char* input, char* units){
+float split(char* input, char* units) {
     return (float)strtol(input, &units, 10);
 }
